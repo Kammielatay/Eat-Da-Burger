e168e839eda79dd5ba72020ee3299411f5dc811b
kammielatay <kameshialatay@gmail.com>
2021-02-16 11:01:58 -0700

    adding default value + inserting seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,15 @@
 --- In this file, write insert queries to populate the burgers table with at least three entries. ---
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Wave", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Hawaiian", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Impossible", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Turkey", true);
+
